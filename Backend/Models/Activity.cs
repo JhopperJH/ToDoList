99069cd19cbd70a997ac4514616ff7888a92528b
JhopperJH <85827152+JhopperJH@users.noreply.github.com>
2025-03-21 10:41:29 +0700

    Add files via upload

---
+++ Backend/Models/Activity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDo.Models;
+
+public partial class Activity
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public DateTime Deadline { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
